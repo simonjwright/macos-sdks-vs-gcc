6d06080abd3d5b6992c8b202af321a2740073dfe
Simon Wright <simon@pushface.org>
2019-06-06 11:54:40 +0100

    Added test code.

---
+++ hello.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{
+  std::cout << "hello world from C++.\n";
+  return 0;
+}
---
+++ hello.f
@@ -0,0 +1,4 @@
+      program hello
+      print *, 'hello world from Fortran.'
+      end
+      
